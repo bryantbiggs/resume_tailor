dc3906cc2f2bfcbf34beaed750a48ecdb4cb0e99
Bryant Biggs <bryantbiggs@users.noreply.github.com>
2016-08-14 13:34:15 -0400

    fixed quotes to let app run correctly

--- flaskapp/application.py
+++ flaskapp/application.py
@@ -3,9 +3,9 @@ from flask import Flask
 application = Flask(__name__)
 application.debug = True
 
-@application.route(‘/’, methods=[‘GET’])
+@application.route('/', methods=['GET'])
 def hello():
-    return ‘<p>Hello world</p>’
+    return '<p>Hello world</p>'
 
-if __name__ == “__main__”:
+if __name__ == '__main__':
     application.run()
